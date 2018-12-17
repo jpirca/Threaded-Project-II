c4afbe33375c852fb53c309f312ffa46227edc42
Jonathan Pirca Chang <730134@ACDM.DS.SAIT.CA>
2018-12-17 09:34:58 -0700

    Main Menu
    First Load
    Menu and Login template

---
+++ ThreadedProjectII/frmMain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreadedProjectII
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            //this.Enabled = false;
+            //this.IsMdiContainer = true;
+            frmLogin Form1 = new frmLogin();
+            Form1.MdiParent = this;
+            //Form2.Enabled = true;
+            //Form2.BringToFront();
+            menuMain.Enabled = false;
+            Form1.Show();
+            Form1.Activate();
+        }
+    }
+}
