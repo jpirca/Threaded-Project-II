2da48d38c11af378dc1914e98d3d906d396ff97d
jeremiahlobo <jeremiahluislobo@gmail.com>
2019-01-08 15:58:16 -0700

    Create froms for Adding products and editing products

--- ThreadedProjectII/frmEditProducts.Designer.cs
+++ ThreadedProjectII/frmEditProducts.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             this.btnCancelPkg = new System.Windows.Forms.Button();
-            this.btnNextPkg = new System.Windows.Forms.Button();
+            this.btnEditProducts = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listEditProducts = new System.Windows.Forms.ListBox();
             this.label10 = new System.Windows.Forms.Label();
             this.lblTitle = new System.Windows.Forms.Label();
             this.SuspendLayout();
@@ -40,7 +40,7 @@
             // btnCancelPkg
             // 
             this.btnCancelPkg.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancelPkg.Location = new System.Drawing.Point(298, 322);
+            this.btnCancelPkg.Location = new System.Drawing.Point(361, 376);
             this.btnCancelPkg.Name = "btnCancelPkg";
             this.btnCancelPkg.Size = new System.Drawing.Size(75, 23);
             this.btnCancelPkg.TabIndex = 41;
@@ -48,22 +48,22 @@
             this.btnCancelPkg.UseVisualStyleBackColor = true;
             this.btnCancelPkg.Click += new System.EventHandler(this.btnCancelPkg_Click);
             // 
-            // btnNextPkg
+            // btnEditProducts
             // 
-            this.btnNextPkg.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNextPkg.Location = new System.Drawing.Point(203, 322);
-            this.btnNextPkg.Name = "btnNextPkg";
-            this.btnNextPkg.Size = new System.Drawing.Size(75, 23);
-            this.btnNextPkg.TabIndex = 40;
-            this.btnNextPkg.Text = "&Edit";
-            this.btnNextPkg.UseVisualStyleBackColor = true;
-            this.btnNextPkg.Click += new System.EventHandler(this.btnNextPkg_Click);
+            this.btnEditProducts.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditProducts.Location = new System.Drawing.Point(266, 376);
+            this.btnEditProducts.Name = "btnEditProducts";
+            this.btnEditProducts.Size = new System.Drawing.Size(75, 23);
+            this.btnEditProducts.TabIndex = 40;
+            this.btnEditProducts.Text = "&Edit";
+            this.btnEditProducts.UseVisualStyleBackColor = true;
+            this.btnEditProducts.Click += new System.EventHandler(this.btnNextPkg_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(235, 115);
+            this.label2.Location = new System.Drawing.Point(298, 169);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(58, 16);
             this.label2.TabIndex = 38;
@@ -73,38 +73,27 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(53, 115);
+            this.label1.Location = new System.Drawing.Point(116, 169);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(54, 16);
             this.label1.TabIndex = 37;
             this.label1.Text = "Product";
             // 
-            // listBox1
+            // listEditProducts
             // 
-            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Items.AddRange(new object[] {
-            "Product 01\t\tSupplier A",
-            "Product 01\t\tSupplier B",
-            "Product 01\t\tSupplier C",
-            "Product 02\t\tSupplier A",
-            "Product 02\t\tSupplier C",
-            "Product 03\t\tSupplier B",
-            "Product 03\t\tSupplier D",
-            "Product 03\t\tSupplier E",
-            "Product 03\t\tSupplier F",
-            "Product 04\t\tSupplier G"});
-            this.listBox1.Location = new System.Drawing.Point(34, 145);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(331, 132);
-            this.listBox1.TabIndex = 36;
+            this.listEditProducts.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.listEditProducts.FormattingEnabled = true;
+            this.listEditProducts.ItemHeight = 16;
+            this.listEditProducts.Location = new System.Drawing.Point(105, 188);
+            this.listEditProducts.Name = "listEditProducts";
+            this.listEditProducts.Size = new System.Drawing.Size(331, 132);
+            this.listEditProducts.TabIndex = 36;
             // 
             // label10
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(63, 9);
+            this.label10.Location = new System.Drawing.Point(126, 63);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(253, 42);
             this.label10.TabIndex = 35;
@@ -114,7 +103,7 @@
             // 
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.Location = new System.Drawing.Point(7, 75);
+            this.lblTitle.Location = new System.Drawing.Point(70, 129);
             this.lblTitle.Name = "lblTitle";
             this.lblTitle.Size = new System.Drawing.Size(339, 16);
             this.lblTitle.TabIndex = 34;
@@ -124,16 +113,16 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(396, 355);
+            this.ClientSize = new System.Drawing.Size(531, 438);
             this.Controls.Add(this.btnCancelPkg);
-            this.Controls.Add(this.btnNextPkg);
+            this.Controls.Add(this.btnEditProducts);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.listEditProducts);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.lblTitle);
             this.Name = "frmEditProducts";
-            this.Text = "frmEditProducts";
+            this.Text = "Edit Product";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -142,10 +131,10 @@
         #endregion
 
         private System.Windows.Forms.Button btnCancelPkg;
-        private System.Windows.Forms.Button btnNextPkg;
+        private System.Windows.Forms.Button btnEditProducts;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listEditProducts;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label lblTitle;
     }
