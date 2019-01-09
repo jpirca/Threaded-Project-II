853317aa1229c758a8f14ac74b6ed99aefefe77d
islammtci <44779168+islammtci@users.noreply.github.com>
2019-01-08 21:55:31 -0700

    TravelExperstWindowsApp added by Muhammad
    TravelExpertsWindowsApp is added with class libraries, test unint and and windows form project. On class is added for the sake of model. refernces are already set

---
+++ TravelExpertsWindowsApp/UnitTestTravelExperts/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestTravelExperts")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestTravelExperts")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7160d138-0f5e-4be5-a504-455a787e84dc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TravelExpertsWindowsApp/UnitTestTravelExperts/UnitTestTravelExperts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestTravelExperts
+{
+    [TestClass]
+    public class UnitTestTravelExperts
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
