9c9568ee063d166ea173bf1149866baaf218a817
jeremiahlobo <jeremiahluislobo@gmail.com>
2019-01-15 12:35:47 -0700

    commit changes

--- Threaded-ProjectLib/BaseADO.cs
+++ Threaded-ProjectLib/BaseADO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -13,8 +14,32 @@ namespace ThreadedProjectLib
      */
     class BaseADO 
     {
-        // database connection variable
-        public SqlConnection cnn;
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(GetConnectionString());
+        }
+
+        static private string GetConnectionString()
+        {
+            // To avoid storing the connection string in your code, 
+            // you can retrieve it from a configuration file, using the 
+            // System.Configuration.ConfigurationManager.ConnectionStrings property 
+            //Connects to the App config 
+
+            ConnectionStringSettingsCollection settings = ConfigurationManager.ConnectionStrings;
+
+            if (settings != null)
+            {
+                foreach (ConnectionStringSettings cs in settings)
+                {
+                    return cs.ConnectionString;
+                }
+                //string connectionToString = @"Data Source=WIN-50GP30FGO75;Initial Catalog=Demodb;User ID=sa;Password=demol23";
+            }
+
+            return null;
+
+        }
 
         /* Get List of Suppliers from database */
         public List<Object> SelectData(string tableName, List<string> selectColNames = null, IDictionary<string, string> conditions = null)
