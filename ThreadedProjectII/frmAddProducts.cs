e53ab55348ea7d59324b95fe8613396c3b5fdb8b
jeremiahlobo <jeremiahluislobo@gmail.com>
2019-01-15 12:38:23 -0700

    Merge branch 'master' of https://github.com/jpirca/Threaded-Project-II

---
+++ ThreadedProjectII/frmAddProducts.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreadedProjectII
+{
+    public partial class frmAddProducts : Form
+    {
+        public frmAddProducts()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddProduct_Click(object sender, EventArgs e)
+        {
+            string addProductName = txtAddProductName.Text;
+
+            //Validate product name 
+
+            //Insert Product Name
+
+        }
+    }
+}
