616964e6f21abdc70475adc792170b71be984fdf
Jeremiah Lobo <693235@ACDM.DS.SAIT.CA>
2019-01-15 18:39:48 -0700

    Base DB Updated and fixed namespacing

--- Threaded-ProjectLib/Product.cs
+++ Threaded-ProjectLib/Product.cs
@@ -17,7 +17,7 @@ namespace ThreadedProjectLib
         private int _productId;
         private string _productName;
 
-        List<Product> Products = new List<Product>();
+       
 
         //constructor
         public Product() { }
@@ -31,108 +31,7 @@ namespace ThreadedProjectLib
         public override string ToString() => "" + this._productName;
 
         //Get all the products
-        public List<Product> getAllProducts()
-        {
-            string queryString =
-                "SELECT ProductID, ProdName FROM dbo.Products";
-            using (SqlConnection connection = base.GetConnection())
-            {
-                SqlCommand command = new SqlCommand(
-                    queryString, connection);
-                //open the connection
-                connection.Open();
-                SqlDataReader reader = command.ExecuteReader();
-                try
-                {
-                    while (reader.Read())
-                    {
-                        Products.Add(new Product(Convert.ToInt32(reader[0]), reader[1].ToString()));
-                    }
-                }
-                finally
-                {
-                    // Always call Close when done reading.
-                    reader.Close();
-                }
-
-                return Products;
-            }
-        }
-
-        //Add a product with  prepare
-        public void AddProduct()
-        {
-            using (SqlConnection connection = base.GetConnection())
-            {
-                connection.Open();
-                SqlCommand command = connection.CreateCommand();
-                SqlTransaction transaction;
-
-                // Start a local transaction.
-                transaction = connection.BeginTransaction("SimpleTransaction");
-
-                // Must assign both transaction object and connection 
-                // to Command object for a pending local transaction
-                command.Connection = connection;
-                command.Transaction = transaction;
-
-                try
-                {
-                    command.CommandText =
-                        "SET IDENTITY_INSERT dbo.Products ON;" +
-                        "Insert into Products ( ProdName ) VALUES (@pname)" +
-                        "SET IDENTITY_INSERT dbo.Products OFF;";
-                    command.Prepare();
-                    //command.Parameters.AddWithValue("@pid", "");
-                    command.Parameters.AddWithValue("@pname", "abc");
-                    command.ExecuteNonQuery();
-
-                    // Attempt to commit the transaction.
-                    transaction.Commit();
-                    Console.WriteLine("Records are written to database.");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Commit Exception Type: {0}", ex.GetType());
-                    Console.WriteLine("  Message: {0}", ex.Message);
-
-                    // Attempt to roll back the transaction. 
-                    try
-                    {
-                        transaction.Rollback();
-                    }
-                    catch (Exception ex2)
-                    {
-                        // This catch block will handle any errors that may have occurred 
-                        // on the server that would cause the rollback to fail, such as 
-                        // a closed connection.
-                        Console.WriteLine("Rollback Exception Type: {0}", ex2.GetType());
-                        Console.WriteLine("  Message: {0}", ex2.Message);
-                    }
-                }
-            }
-        }
-
-
-        public void TestingConnection()
-        {
-            /// only for testing purposes remove once done
-            SqlConnection connection;
-            using (connection = base.GetConnection())
-            {
-                connection.Open();
-                Console.WriteLine("ServerVersion: {0}", connection.ServerVersion);
-                Console.WriteLine("State: {0}", connection.State);
-            }
-
-            if (connection != null)
-            {
-                Console.WriteLine(connection);
-                Console.WriteLine();
-                Console.WriteLine("State: {0}", connection.State);
-            }
-
-        }
+        
         // Copy Data From Database Element
         public void CopyDataFromDBElement(IDictionary<string, string> element)
         {
--- Threaded-ProjectLib/ProductADO.cs
+++ Threaded-ProjectLib/ProductADO.cs
@@ -7,9 +7,113 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using ThreadedProjectLib;
 
-namespace Threaded_ProjectLib
+namespace ThreadedProjectLib
 {
     class ProductADO : BaseDB
     {
+
+        List<Product> Products = new List<Product>();
+        public List<Product> getAllProducts()
+        {
+            string queryString =
+                "SELECT ProductID, ProdName FROM dbo.Products";
+            using (SqlConnection connection = GetConnection())
+            {
+                SqlCommand command = new SqlCommand(
+                    queryString, connection);
+                //open the connection
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                try
+                {
+                    while (reader.Read())
+                    {
+                        Products.Add(new Product(Convert.ToInt32(reader[0]), reader[1].ToString()));
+                    }
+                }
+                finally
+                {
+                    // Always call Close when done reading.
+                    reader.Close();
+                }
+
+                return Products;
+            }
+        }
+
+        //Add a product with  prepare
+        public void AddProduct()
+        {
+            using (SqlConnection connection = base.GetConnection())
+            {
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                SqlTransaction transaction;
+
+                // Start a local transaction.
+                transaction = connection.BeginTransaction("SimpleTransaction");
+
+                // Must assign both transaction object and connection 
+                // to Command object for a pending local transaction
+                command.Connection = connection;
+                command.Transaction = transaction;
+
+                try
+                {
+                    command.CommandText =
+                        "SET IDENTITY_INSERT dbo.Products ON;" +
+                        "Insert into Products ( ProdName ) VALUES (@pname)" +
+                        "SET IDENTITY_INSERT dbo.Products OFF;";
+                    command.Prepare();
+                    //command.Parameters.AddWithValue("@pid", "");
+                    command.Parameters.AddWithValue("@pname", "abc");
+                    command.ExecuteNonQuery();
+
+                    // Attempt to commit the transaction.
+                    transaction.Commit();
+                    Console.WriteLine("Records are written to database.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Commit Exception Type: {0}", ex.GetType());
+                    Console.WriteLine("  Message: {0}", ex.Message);
+
+                    // Attempt to roll back the transaction. 
+                    try
+                    {
+                        transaction.Rollback();
+                    }
+                    catch (Exception ex2)
+                    {
+                        // This catch block will handle any errors that may have occurred 
+                        // on the server that would cause the rollback to fail, such as 
+                        // a closed connection.
+                        Console.WriteLine("Rollback Exception Type: {0}", ex2.GetType());
+                        Console.WriteLine("  Message: {0}", ex2.Message);
+                    }
+                }
+            }
+        }
+
+
+        public void TestingConnection()
+        {
+            /// only for testing purposes remove once done
+            SqlConnection connection;
+            using (connection = base.GetConnection())
+            {
+                connection.Open();
+                Console.WriteLine("ServerVersion: {0}", connection.ServerVersion);
+                Console.WriteLine("State: {0}", connection.State);
+            }
+
+            if (connection != null)
+            {
+                Console.WriteLine(connection);
+                Console.WriteLine();
+                Console.WriteLine("State: {0}", connection.State);
+            }
+
+        }
     }
 }
